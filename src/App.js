61565c60a3799771a499cec30c40fb8e5acc1f40
Isabella Lopes <icmlopes@gmail.com>
2022-09-26 15:04:19 -0300

    atualização

--- src/App.js
+++ src/App.js
@@ -1,47 +1,54 @@
 import palavras from "./palavras"
-
+import React, { useState } from "react"
+import imagem0 from "./img/forca0.png"
 
 
 
 function Alfabeto(props) {
-
     return (
         <li className="letra">{props.index}</li>
     )
+}
+        
+function Selecionar(props) {
+    return (
+        console.log(props.index)
+    )
+}
+
+function Sortear() {
 
 }
 
 
-
 export default function App() {
-
-
 
     const listaAlfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
         "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
+  
     return (
-        <div>
+        <>  
             <div>
-                <div className="centro">
+                <div className="centro"> 
                     <div>
-                        <img src="./assets/forca0.png" />
+                        <img src="imagem0" />
                     </div>
                     <div>
                         <button>Sortear Palavra</button>
                         <div className="traco">a</div>
                     </div>
                 </div>
-                <ul className="baralho">
-                    {listaAlfabeto.map((letra) => <Alfabeto index={letra} />)}
-                </ul>
-
+                <div className="baralho" >
+                    <button onClick={Selecionar}>
+                        {listaAlfabeto.map((letra, index) => <Alfabeto index={letra} key={index} />)}
+                    </button>
+                </div>
                 <div className="final">
                     <p>Já sei a palavra!</p>
                     <input type="text" placeholder="" />
                     <button> Chutar </button>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom";
 import App from "./App"
+import "./styles/reset.css"
+import "./styles/style.css"
 import styled from 'styled-components'
 
 ReactDOM.render(<App />, document.querySelector(".root"))
